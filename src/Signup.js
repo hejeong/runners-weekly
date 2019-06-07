a9a15aa9311c1f43077211181175e6c2f1f87183
hejeong <jonhong777@gmail.com>
2019-06-07 17:40:47 -0400

    Add state to signup component and handleOnChanges for form components

--- src/Signup.js
+++ src/Signup.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 
 class Signup extends Component {
+    constructor(){
+        super()
+        this.state = {
+            name: "",
+            username: ""
+        }
+    }
+    handleOnChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
     render(){
         return(
             <div>
                 <form>
                     <h1>Sign Up</h1>
                     <label for="name">Name:</label>
-                    <input type="text" name="name"  /> <br /><br />
+                    <input type="text" name="name" value={this.state.name} onChange={this.handleOnChange} /> <br /><br />
                     <label for="username">Username:</label>
-                    <input type="text" name="username"  />
+                    <input type="text" name="username" value={this.state.username} onChange={this.handleOnChange} />
                 </form>
             </div>
         )
