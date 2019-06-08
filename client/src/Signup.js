438d2cd5c47ee57a7be29a6e1cb464cbef8d5e35
hejeong <jonhong777@gmail.com>
2019-06-07 23:13:43 -0400

    Add inputs on signup form to match users schema

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -5,7 +5,9 @@ class Signup extends Component {
         super()
         this.state = {
             name: "",
-            username: ""
+            username: "",
+            password: "",
+            email: ""
         }
     }
     handleOnChange = (event) => {
@@ -13,15 +15,26 @@ class Signup extends Component {
             [event.target.name]: event.target.value
         })
     }
+
     render(){
         return(
             <div>
-                <form>
+                <form >
                     <h1>Sign Up</h1>
-                    <label for="name">Name:</label>
+                    <label for="name">Name:</label> <br />
                     <input type="text" name="name" value={this.state.name} onChange={this.handleOnChange} /> <br /><br />
-                    <label for="username">Username:</label>
-                    <input type="text" name="username" value={this.state.username} onChange={this.handleOnChange} />
+
+                    <label for="username">Username:</label> <br />
+                    <input type="text" name="username" value={this.state.username} onChange={this.handleOnChange} /> <br /><br />
+
+                    <label for="password">Password:</label> <br />
+                    <input type="password" name="password" value={this.state.password} onChange={this.handleOnChange} /> <br /><br />
+
+                    <label for="email">E-mail:</label> <br />
+                    <input type="email" name="email" value={this.state.email} onChange={this.handleOnChange} /> <br /><br />
+
+                    <input type="submit" name="submit" value="Submit"/>
+
                 </form>
             </div>
         )
