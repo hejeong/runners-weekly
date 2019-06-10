e64a08786637999028dce7cf5687080082060d0a
hejeong <jonhong777@gmail.com>
2019-06-10 15:01:08 -0400

    Add front-end login form

---
+++ client/src/Login.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    constructor(){
+        super()
+        this.state={
+            username: '',
+            password: '',
+            redirect: false
+        }
+    }
+
+    handleOnChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+    handleOnSubmit = (event) => {
+        event.preventDefault()
+        fetch(`http://localhost:8000/api/login`, {
+            method: 'POST',
+            headers: {
+				"Content-Type": 'application/json'
+            },
+            body: {
+                user: {
+                    username: this.state.username,
+                    password: this.state.password
+                }
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+    }
+    render(){
+        if(this.state.redirect){
+            return <Redirect to='/' />
+        }
+        return(
+            <div>
+                 <form onSubmit={this.handleOnSubmit}>
+                    <h1>Log In</h1>
+                   
+                    <label > Username:</label> <br />
+                    <input type="text" name="username" value={this.state.username} onChange={this.handleOnChange} /> <br /><br />
+
+                    <label > Password:</label> <br />
+                    <input type="password" name="password" value={this.state.password} onChange={this.handleOnChange} /> <br /><br />
+
+                    <input type="submit" name="submit" value="Submit"/>
+
+                </form>
+            </div>
+        )
+    }
+}
