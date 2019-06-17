94c15729becd8804b2e59efe4e0f720bb8db0310
hejeong <jonhong777@gmail.com>
2019-06-16 20:33:29 -0400

    Create posts model in rails api

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_08_010551) do
+ActiveRecord::Schema.define(version: 2019_06_17_003254) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
