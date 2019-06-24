cf3f36bd9ae4c4e80935063c1f0d02e9b3e04524
hejeong <jonhong777@gmail.com>
2019-06-23 20:20:35 -0400

    Add show route to user at alias /profile and create ajax fetch request for the data and add state to ProfileContainer

--- client/src/components/ProfileContainer.js
+++ client/src/components/ProfileContainer.js
@@ -1,5 +1,18 @@
 import React, { Component } from 'react';
 
 class ProfileContainer extends Component {
-    
+    constructor(props) {
+        super(props)
+        this.state = {
+            userInfo: null
+        }
+    }
+    componentDidMount(){
+        fetch(`http://localhost:8000/api/profile/` + this.props.currentUsername)
+        .then(resp => resp.json())
+        .then(data => this.setState({
+            userInfo: data
+        }))
+        .catch(err => console.error('Error:', err));
+    }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope :api do
     resources :users, only: [:create]
+    get '/profile/:username' => 'users#show'
     post '/login' => 'auth#create'
     post '/logout' => 'auth#delete'
     resources :posts, only: [:create, :index, :show]
