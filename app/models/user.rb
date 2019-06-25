4fd905e7be3645d9ce36f0650b4cf5ba6746d924
hejeong <jonhong777@gmail.com>
2019-06-25 15:17:15 -0400

    Add associations to user and post models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
 
+    has_many :posts
     validates :username, uniqueness: true, presence: true
     validates :email, uniqueness: true, presence: true
     validates :name, presence: true, presence: true
